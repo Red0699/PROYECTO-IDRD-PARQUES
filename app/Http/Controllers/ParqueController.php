79007b0ad9b387190b3df359f500cfd96fba8696
Nicolás Campos <74160263+Red0699@users.noreply.github.com>
2022-12-14 17:24:53 -0500

    Migracioón modulo de parques

---
+++ app/Http/Controllers/ParqueController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Parque;
+use Illuminate\Http\Request;
+
+class ParqueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Parque  $parque
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Parque $parque)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Parque  $parque
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Parque $parque)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Parque  $parque
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Parque $parque)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Parque  $parque
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Parque $parque)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_12_14_222023_create_parques_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parques', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombreParque');
+            $table->string('localidad');
+            $table->integer('area');
+            $table->string('escala');
+            $table->integer('estrato');
+            $table->string('direccion');
+            $table->string('foto')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parques');
+    }
+};
