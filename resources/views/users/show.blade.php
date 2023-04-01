82cc705cc46e161882ea50d5450be4984c4a6aeb
Nicolás Campos <74160263+Red0699@users.noreply.github.com>
2023-04-01 10:15:01 -0500

    Pagina ver usuario

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,62 +6,56 @@
     .container {
         width: 80%;
         height: 80%;
-        
     }
 
-    .fotoPerfil .avatar {
-        width: 140px;
-        height: 160px;
+    .card-header {
+        color: white;
+        font-size: 24px;
+        font-weight: bold;
     }
+
+    .card-body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .card-body img {
+        width: 150px;
+        height: 150px;
+        border-radius: 50%;
+        object-fit: cover;
+        margin-bottom: 20px;
+    }
+
+    .card-body h2 {
+        font-size: 32px;
+        margin-bottom: 20px;
+    }
+
+    .card-body p {
+        font-size: 18px;
+        margin-bottom: 10px;
+    }
+
+    
 </style>
 
-<div class="container card py-5">
-    <!-------------------- Header ----------------------->
-    <div class="header bg-purple pb-7 pt-5 pt-lg-7 d-flex align-items-center">
-        <!-- Mask -->
-        <span class="mask bg-gradient-default opacity-4"></span>
-        <!-- Header container -->
-        <div class="container-fluid d-flex align-items-center justify-content-center">
-            <div class="row">
-                <div class="col-md-12 col-lg-12">
-                    <h1 class="display-2 text-white">Perfil de usuario</h1>
-                </div>
-            </div>
+<div class="container py-5">
+    <div class="card">
+        <div class="card-header bg-purple text-center">
+            Perfil de usuario
         </div>
-
-    </div>
-    <!-------------------------------------------------------- Contenido --------------------------------------------------------->
-
-    <!---------------- Foto ---------------->
-    <div class="fotoPerfil">
-        <div class="row justify-content-center">
-            <div class="order-lg-2">
-                <div class="card-profile-image">
-                    <a href="#">
-                        @if(auth()->user()->photo == null)
-                        <img src="../assets/img/theme/default-user-image.png" class="avatar">
-                        @else
-                        <img src="{{ auth()->user()->photo }}" class="img-fluid rounded-circle avatar-lg">
-                        @endif
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!------------- Información ------------->
-    <div class="informacion py-8 justify-content-center align-items-center">
-
-        
+        <div class="card-body">
+            <img src="{{ auth()->user()->photo ?? '../assets/img/theme/default-user-image.png' }}" alt="Foto de perfil">
             <h2>{{ $user->name }}</h2>
-            <p><strong>Correo electrónico: </strong>{{ $user->email }}</p>
+            <p><strong>Correo electrónico:</strong> {{ $user->email }}</p>
             <p><strong>ID:</strong> {{ $user->id }}</p>
             <p><strong>Fecha de creación:</strong> {{ $user->created_at }}</p>
             <p><strong>Fecha de actualización:</strong> {{ $user->updated_at }}</p>
-            <a href="" class="btn bg-purple text-white">Ver historial</a>
-        
+            <a href="#" class="btn btn-primary">Ver historial</a>
+        </div>
     </div>
-
 </div>
 
 @endsection
