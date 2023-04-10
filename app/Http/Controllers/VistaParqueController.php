e6273434d25141cba3e13a6644e98e829f81a47b
Nicolas <yncampos@ucundinamarca.edu.co>
2023-04-10 00:41:49 -0500

    Captura de datos y filtros a la lista de parques

--- app/Http/Controllers/VistaParqueController.php
+++ app/Http/Controllers/VistaParqueController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Parque;
 use Illuminate\Http\Request;
 
 class VistaParqueController extends Controller
 {
     //
 
-    public function index(){
-        return view('pages\parques\home\view');
+    public function index(Request $request){
+        $localidad = $request->input('localidad');
+        $escala = $request->input('escala');
+        $parques = Parque::query();
+
+        if($localidad){
+            $parques = $parques->where('localidad', $localidad);
+        }
+        if($escala){
+            $parques = $parques->where('escala', $escala);
+        }
+
+        $parques = $parques->get();
+        return view('pages\parques\home\view', compact(
+            'parques',
+            'localidad',
+            'escala'
+        ));
     }
 }
--- resources/views/pages/parques/home/view.blade.php
+++ resources/views/pages/parques/home/view.blade.php
@@ -3,6 +3,11 @@
 @section('content')
 
 <style>
+    .dataTables_filter input {
+        width: 400px;
+        /* o cualquier ancho deseado */
+    }
+
     /* Estilos generales */
     body {
         font-family: "Open Sans", sans-serif;
@@ -37,13 +42,17 @@
     .table tbody+tbody {
         border-top: 2px solid #dee2e6;
     }
-
 </style>
 
+@push('css')
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css">
+@endpush
+
 <div>
-    <div class="header bg-purple pb-8 pt-5 pt-lg-8 d-flex">
+    <div class="header bg-purple pb-7 pt-5 pt-lg-8 d-flex">
         <div class="container-fluid d-flex align-items-center justify-content-center">
-            <h1 class="text-white">Datos del Parque</h1>
+            <h1 class="text-white ">Lista de parques administrados por el IDRD</h1>
         </div>
     </div>
     <div class="container-fluid mt--7">
@@ -51,39 +60,100 @@
             <div class="col">
                 <div class="card shadow">
                     <div class="card-header border-0">
-                        <h3 class="mb-0">Tabla de Datos</h3>
+                        <form action="{{ route('vista') }}" method="GET">
+                            <div class="row justify-content-center align-items-center">
+                                <div class="col-md-4">
+                                    <h5>Localidad:</h5>
+                                    <select class="form-control" aria-label="Localidad" id="localidad" name="localidad">
+                                        <option value="" selected>Seleccionar localidad</option>
+                                        <option value="Antonio Nariño"{{ $localidad == 'Antonio Nariño' ? 'selected' : '' }}>Antonio Nariño</option>
+                                        <option value="Barrios Unidos"{{ $localidad == 'Barrios Unidos' ? 'selected' : '' }}>Barrios Unidos</option>
+                                        <option value="Bosa"{{ $localidad == 'Bosa' ? 'selected' : '' }}>Bosa</option>
+                                        <option value="Chapinero"{{ $localidad == 'Chapinero' ? 'selected' : '' }}>Chapinero</option>
+                                        <option value="Ciudad Bolívar"{{ $localidad == 'Ciudad Bolívar' ? 'selected' : '' }}>Ciudad Bolívar</option>
+                                        <option value="Engativá"{{ $localidad == 'Engativá' ? 'selected' : '' }}>Engativá</option>
+                                        <option value="Fontibón"{{ $localidad == 'Fontibón' ? 'selected' : '' }}>Fontibón</option>
+                                        <option value="Kennedy"{{ $localidad == 'Kennedy' ? 'selected' : '' }}>Kennedy</option>
+                                        <option value="La Candelaria"{{ $localidad == 'La Candelaria' ? 'selected' : '' }}>La Candelaria</option>
+                                        <option value="Los Mártires"{{ $localidad == 'Los Mártires' ? 'selected' : '' }}>Los Mártires</option>
+                                        <option value="Puente Aranda"{{ $localidad == 'Puente Aranda' ? 'selected' : '' }}>Puente Aranda</option>
+                                        <option value="Rafael Uribe Uribe"{{ $localidad == 'Rafael Uribe Uribe' ? 'selected' : '' }}>Rafael Uribe Uribe</option>
+                                        <option value="San Cristóbal"{{ $localidad == 'San Cristóbal' ? 'selected' : '' }}>San Cristóbal</option>
+                                        <option value="Santa Fe"{{ $localidad == 'Santa Fe' ? 'selected' : '' }}>Santa Fe</option>
+                                        <option value="Suba"{{ $localidad == 'Suba' ? 'selected' : '' }}>Suba</option>
+                                        <option value="Sumapaz"{{ $localidad == 'Sumapaz' ? 'selected' : '' }}>Sumapaz</option>
+                                        <option value="Teusaquillo"{{ $localidad == 'Teusaquillo' ? 'selected' : '' }}>Teusaquillo</option>
+                                        <option value="Tunjuelito"{{ $localidad == 'Tunjuelito' ? 'selected' : '' }}>Tunjuelito</option>
+                                        <option value="Usaquén"{{ $localidad == 'Usaquén' ? 'selected' : '' }}>Usaquén</option>
+                                        <option value="Usme"{{ $localidad == 'Usme' ? 'selected' : '' }}>Usme</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-4">
+                                    <h5>Escala:</h5>
+                                    <select class="form-control" aria-label="Escala" id="escala" name="escala">
+                                        <option value="" selected>Seleccionar escala</option>
+                                        <option value="Parque de Bolsillo" {{ $escala == 'Parque de Bolsillo' ? 'selected' : '' }}>Parque de Bolsillo</option>
+                                        <option value="Parque Metropolitano" {{ $escala == 'Parque Metropolitano' ? 'selected' : '' }}>Parque Metropolitano</option>
+                                        <option value="Parque Vecinal" {{ $escala == 'Parque Vecinal' ? 'selected' : '' }}>Parque Vecinal</option>
+                                        <option value="Parque Zonal" {{ $escala == 'Parque Zonal' ? 'selected' : '' }}>Parque Zonal</option>
+                                        <option value="Parque Regional" {{ $escala == 'Parque Regional' ? 'selected' : '' }}>Parque Regional</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-2 align-self-end">
+                                    <button class="btn bg-purple text-white btn-block" type="submit">
+                                        Filtrar
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                    <div class="table-responsive">
-                        <table class="table align-items-center table-flush">
-                            <thead class="thead-light">
-                                <tr>
-                                    <th scope="col">ID</th>
-                                    <th scope="col">Nombre</th>
-                                    <th scope="col">Ubicación</th>
-                                    <th scope="col">Área (m²)</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Parque Nacional Natural Tayrona</td>
-                                    <td>Santa Marta, Colombia</td>
-                                    <td>58.300</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Parque Nacional Natural Los Nevados</td>
-                                    <td>Caldas, Colombia</td>
-                                    <td>58.300</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>Parque Natural Regional El Faro</td>
-                                    <td>Bogotá, Colombia</td>
-                                    <td>16.000</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table align-items-center table-flush" id="parqueTable">
+                                <thead class="thead-light">
+
+                                    <th>Nombre</th>
+                                    <th>Foto</th>
+                                    <th>Localidad</th>
+                                    <th>Dirección</th>
+                                    <th>Escala</th>
+                                    <th>Area</th>
+                                    <th>Acciones</th>
+
+                                </thead>
+                                <tbody class="list">
+                                    @forelse ($parques as $parque)
+
+                                    <tr>
+                                        <td>{{ $parque->nombreParque }}</td>
+                                        @if($parque->foto == null)
+
+                                        <td>
+                                            <span class="badge badge-danger">Sin Foto</span>
+                                        </td>
+                                        @else
+                                        <td>
+                                            <img src=" {{ asset('images/parques').'/'.$parque->foto }} " alt="" width="100">
+                                        </td>
+                                        @endif
+                                        <th>{{ $parque->localidad }}</th>
+                                        <th>{{ $parque->direccion }}</th>
+                                        <th>{{ $parque->escala }}</th>
+                                        <th>{{ $parque->area }}m<sup>2</sup></th>
+
+                                        <td class="td-actions">
+                                            <a href="#" class="btn bg-purple text-white"><i class="fas fa-eye"></i></a>
+                                        </td>
+                                    </tr>
+                                    @empty
+
+                                    <tr>
+                                        <td colspan="2">Sin registros.</td>
+                                    </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -92,3 +162,39 @@
 </div>
 
 @endsection
+
+@push('js')
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.0/jszip.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+
+
+<script>
+    $(document).ready(function() {
+        $('#parqueTable').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
+                'pageLength',
+            ],
+            language: {
+                lengthMenu: 'Mostrando _MENU_ registros por página',
+                zeroRecords: 'No hay registros para mostrar',
+                info: 'Mostrando página _PAGE_ de _PAGES_',
+                infoEmpty: 'No hay registros...',
+                infoFiltered: '(filtrando de _MAX_ registros disponibles)',
+                sSearch: 'Buscar',
+                'paginate': {
+                    'previous': '<i class="fas fa-light fa-arrow-left"></i>',
+                    'next': '<i class="fas fa-light fa-arrow-right"></i>'
+                },
+                buttons: {
+                    pageLength: 'Mostrando %d filas'
+                },
+            },
+        });
+    });
+</script>
+@endpush
