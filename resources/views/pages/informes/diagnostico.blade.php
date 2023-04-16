01a93d4c0b0837b82428b8d2feb8ef723f0f7aa2
Nicolas <yncampos@ucundinamarca.edu.co>
2023-04-16 10:43:45 -0500

    Cambios

--- resources/views/pages/informes/diagnostico.blade.php
+++ resources/views/pages/informes/diagnostico.blade.php
@@ -9,5 +9,5 @@
     }
 </style>
 
-<iframe class="informe" title="Report Section" src="https://app.powerbi.com/view?r=eyJrIjoiYTNkMWM1YjMtM2JjZi00MjcyLWFiMGQtYjBhNzBlMzJiMTE3IiwidCI6IjA3ZGE2N2EwLTFmNDMtNGU4Yy05NzdmLTVmODhiNjQ3MGVlNiIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+<iframe title="InformesProyecto" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=d826e683-bc0a-4a1a-8dbf-09a1521a0eb4&autoAuth=true&ctid=07da67a0-1f43-4e8c-977f-5f88b6470ee6" frameborder="0" allowFullScreen="true"></iframe>
 @endsection
