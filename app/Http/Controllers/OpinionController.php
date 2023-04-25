432796499d364c3124be76d917c020e47dd60e94
Nicolas <yncampos@ucundinamarca.edu.co>
2023-04-24 19:09:56 -0500

    Migracion tabla de buzon de opiniones y sugerencias

---
+++ app/Http/Controllers/OpinionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Opinion;
+use Illuminate\Http\Request;
+
+class OpinionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Opinion  $opinion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Opinion $opinion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Opinion  $opinion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Opinion $opinion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Opinion  $opinion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Opinion $opinion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Opinion  $opinion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Opinion $opinion)
+    {
+        //
+    }
+}
