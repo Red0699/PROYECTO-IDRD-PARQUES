af8b6bfdd21f880ca3f79940d6adeedb4040c9d9
Nicolás Campos <74160263+Red0699@users.noreply.github.com>
2023-05-17 09:48:19 -0500

    Se ajusta el formulario de edicion de usuario

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -5,23 +5,26 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                <form action="{{ route('user.update', $user->id) }}" method="post" class="form-horizontal">
-                    @csrf
-                    @method('PUT')
-                    <div class="card text-center">
-                        <div class="card-header text-white card-header-primary bg-purple">
-                            Editar Usuario
-                        </div>
-                        <div class="card-body">
+                <div class="card text-center">
+                    <div class="card-header text-white card-header-primary bg-purple">
+                        Editar Usuario
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('user.update', $user->id) }}" method="post" class="form-horizontal">
+                            @csrf
+                            @method('PUT')
+
                             <div class="row">
                                 <label for="name" class="col-sm-2 col-form-label">Nombre</label>
-                                <div class="col-sm-7">
-                                    <input type="text" class="form-control" name="name" value="{{ old('name', $user->name) }}" autofocus>
+                                <div class="col-sm-8">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="name" value="{{ old('name', $user->name) }}" autofocus>
+                                    </div>
                                 </div>
                             </div>
                             <div class="row">
                                 <label for="email" class="col-sm-2 col-form-label">Correo electrónico</label>
-                                <div class="col-sm-7">
+                                <div class="col-sm-8">
                                     <div class="form-group">
                                         <input type="text" class="form-control" name="email" value="{{ old('email', $user->email) }}" autofocus>
                                     </div>
@@ -30,7 +33,7 @@
 
                             <div class="row">
                                 <label for="password" class="col-sm-2 col-form-label">Contraseña</label>
-                                <div class="col-sm-7">
+                                <div class="col-sm-8">
                                     <div class="form-group">
                                         <input type="password" class="form-control" name="password" value="{{ old('password', $user->password) }}" autofocus>
                                     </div>
@@ -39,12 +42,12 @@
 
                             <div class="row">
                                 <label for="rol" class="col-sm-2 col-form-label">Seleccione un Rol</label>
-                                <div class="col-sm-7">
+                                <div class="col-sm-8">
                                     <div class="form-group">
                                         <select class="form-control" name="roles">
-                                        <option value="">-- Seleccione --</option>
+                                            <option value="">-- Seleccione --</option>
                                             @foreach ($roles as $rol)
-                                            
+
                                             @if ($user->roles[0]->id == $rol->id)
                                             <option value="{{$rol->id}}" selected>{{$rol->name}}</option>
                                             @else
@@ -55,15 +58,18 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <!--Footer-->
-                        <div class="card-footer ml-auto mr-auto">
-                            <button type="submit" class="btn btn-primary">Actualizar</button>
-                            <a href="/user" class="btn btn-light">Volver</a>
-                        </div>
-                        <!--End footer-->
+
+                            <!--Footer-->
+                            <div class="card-footer ml-auto mr-auto">
+                                <button type="submit" class="btn btn-primary">Actualizar</button>
+                                <a href="/user" class="btn bg-purple text-white">Volver</a>
+                            </div>
+                            <!--End footer-->
+
+                        </form>
+
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     </div>
